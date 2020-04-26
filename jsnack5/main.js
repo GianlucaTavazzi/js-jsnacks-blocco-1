bdb33e639223e3cc4e396074c5aa64b9ac79c254
Gianluca Tavazzi <gianluca.tavazzi@gmail.com>
2020-04-26 18:22:30 +0200

    correzione

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -3,7 +3,7 @@ var numero_utente = parseInt(prompt('inserisci un numero'))
 //creo un ciclo for da 1 al numero dato dall'utente
 for (var i = 1; i <= numero_utente ; i++) {
     //elevo al quadrato i numeri da 1 a n
-    var quadrato = Math.pow(i,2)
+    var quadrato = Math.pow(i,3)
     //stampo il risultato
     console.log(quadrato);
 }
